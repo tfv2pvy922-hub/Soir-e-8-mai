df01b6753cfb14f603a188e4e68e58e105e8a7ad
tfv2pvy922-hub <tfv2pvy922@privaterelay.appleid.com>
2026-05-03 15:45:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.CR3PSCL_CONFIG = {
-  SUPABASE_URL: "https://ruhbmpingkcspmzdmuzc.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://ruhbmpingkcspmzdmuzc.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1aGJtcGluZ2tjc3BtemRtdXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4MDc1MDksImV4cCI6MjA5MzM4MzUwOX0.GzftqZTeuDqfqfHyRJYReUW1bvqbbIvm56WR5PFZR6U",
   ADMIN_CODE: "0805",
 };
